78f8afde91af3f13b02c2d08516319dbefac9864
Soroush <shuoros@yahoo.com>
2023-08-28 15:40:11 +0200

    IMPLEMENT[downloadAvatar]

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -1,4 +1,8 @@
-function refreshAvatar() {
+$(function() {
+   fetchAvatar();
+});
+
+function fetchAvatar() {
     const req = new XMLHttpRequest();
     req.getResponseHeader('Content-Type', 'image/png');
     const url = new URL(window.location.origin + '/api/generate/avatar');
@@ -8,16 +12,19 @@ function refreshAvatar() {
 
     req.onload = (event) => {
       const data = req.response;
-      handleRefreshAvatarResponse(data);
+      handleFetchAvatarResponse(data);
     };
 
     req.send();
 }
 
-function handleRefreshAvatarResponse(data) {
+function handleFetchAvatarResponse(data) {
     const unit8Array = new Uint8Array(data);
     const prepareToEncode = String.fromCharCode.apply(null, unit8Array);
     const dataInBase64 = btoa(prepareToEncode);
     document.getElementById("avatar").src = "data:image/png;base64," + dataInBase64;
+    var downloadAvatarButton = document.getElementById("download-avatar");
+    downloadAvatarButton.href = "data:image/png;base64," + dataInBase64;
+    downloadAvatarButton.download = "avatar.png";
 }
 
