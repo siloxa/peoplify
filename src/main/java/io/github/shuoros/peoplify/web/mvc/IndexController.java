0d5445c89339f5456fad4f6c69c00f1630085d04
Soroush <shuoros@yahoo.com>
2023-08-28 14:35:30 +0200

    IMPLEMENT[mvc for index]

---
+++ src/main/java/io/github/shuoros/peoplify/web/mvc/IndexController.java
@@ -0,0 +1,13 @@
+package io.github.shuoros.peoplify.web.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "")
+    public String index() {
+        return "index";
+    }
+}
